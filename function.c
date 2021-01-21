db3ca6fdf6bdf60ebacc2271ef645731af461f3a
atulcuelogic <77664099+atulcuelogic@users.noreply.github.com>
2021-01-21 11:32:40 +0530

    Update function.c

--- function.c
+++ function.c
@@ -2,13 +2,17 @@
 int add(a,b) 
   return(a+b)
 
+int subtract(a,b)
+  return(a-b)
+  
+
+int multiply(a,b)
+  return(a*b)
+
 int positiveno(n)
 {
 if a<0
   return(-a)
 else 
   return(a)
-
-
-int subtract(a,b)
-  return(a-b)
+ }
